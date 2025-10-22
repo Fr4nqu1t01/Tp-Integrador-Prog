01e4e50b9343d467b4f223aedb1baca2ad54034c
unknown <constanzad.vivas@gmail.com>
2025-10-21 21:03:30 -0300

    Agrego el arreglo con los estudiantes y la constante para usar prompt-sync

--- Control-Estudiantes.js
+++ Control-Estudiantes.js
@@ -1,1 +1,19 @@
-#//TP FINAL PROG
+//TP FINAL PROG
+const prompt = require (`prompt-sync`)();
+
+let estudiantes = [{
+    nombre: "Franco",
+    notas: [],
+    asistencias: [] 
+},
+{
+    nombre: "Constanza",
+    notas: [],
+    asistencias: []
+},
+{
+    nombre: "Lucas souza",
+    notas: [],
+    asistencias: []
+}];
+
