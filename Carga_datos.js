8ccdea4d97030841092ceecca60f339f1bb3090e
Fr4nqu1t01 <villalbafranco482@gmail.com>
2025-11-01 13:49:34 -0300

    modifico el archivo Control-Estudiantes.js para que sea el programa MAIN. agrego funcion mostrarAlumnos en Carga_datos.js

--- Carga_datos.js
+++ Carga_datos.js
@@ -10,3 +10,19 @@ export function guardarDatos(datos) {
   const texto = JSON.stringify(datos, null, 2);
   fs.writeFileSync(rutaAlumnos, texto, "utf8");
 }
+
+export function mostrarAlumnos() {
+  const datos = leerDatos();
+
+  if (datos.alumnos.length === 0) {
+    console.log("No hay alumnos registrados.");
+    return;
+  }
+
+  console.log(`Alumnos registrados:`);
+  const alumnos = datos.alumnos;
+  for (let i = 0; i < alumnos.length; i++) {
+    console.log(`${i + 1}- ${alumnos[i].nombre}`);
+  }
+  console.log("====================================================");
+}
